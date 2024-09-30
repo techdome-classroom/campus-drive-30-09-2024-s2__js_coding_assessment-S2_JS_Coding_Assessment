3afd54212c7976b3ed73190da6e26571c9abbe25
Yashraj360 <yashraj.sahu.jobsdoor360@gmail.com>
2024-09-30 13:56:06 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -8,14 +8,14 @@ var isValid = function(s) {
         ']': '[',
         '}': '{'
     };
-    // Stack to hold the opening brackets
+   
     const stack = [];
 
-    // Iterate over each character in the string
+    
     for (let char of s) {
-        // If char is a closing bracket
+        
         if (bracketMap[char]) {
-            // Pop the top of the stack if not empty, otherwise assign a dummy value
+            
             const topElement = stack.length > 0 ? stack.pop() : '#';
            
             if (topElement !== bracketMap[char]) {
