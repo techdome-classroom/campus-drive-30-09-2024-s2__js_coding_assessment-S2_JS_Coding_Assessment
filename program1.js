0f5887d84f5c2c64ddc35adf77996938482caa1c
abhisheklodha001 <abhishek.lodha.jobsdoor360@gmail.com>
2024-09-30 14:17:01 +0530

    complete

--- program1.js
+++ program1.js
@@ -4,8 +4,20 @@
  */
 var isValid = function(s) {
     
-};
+    let stack = [];
+    for (let idx = 0; idx < s.length; idx++) {
+        if (s[idx] == '{') {
+            stack.push('}');
+        } else if (s[idx] == '[') {
+            stack.push(']');
+        } else if (s[idx] == '(') {
+            stack.push(')');
+        }
+        else if (stack.pop() !== s[idx]) {
+            return false;
+        }
+    }
+    return !stack.length;
+}
 
 module.exports = { isValid };
-
-
