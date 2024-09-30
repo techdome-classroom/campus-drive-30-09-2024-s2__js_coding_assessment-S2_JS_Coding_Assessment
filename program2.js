70ed55a24e64afff4348e5846d3cc44ce985f733
Yashraj360 <yashraj.sahu.jobsdoor360@gmail.com>
2024-09-30 14:10:49 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,29 +3,7 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    unordered_map<char, int> romanValues = {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000}
-    };
-
-    int result = 0;
-
-    for (int i = s.length() - 1; i >= 0; i--) {
-        int currValue = romanValues[s[i]];
-
-        if (i < s.length() - 1 && currValue < romanValues[s[i + 1]]) {
-            result -= currValue;
-        } else {
-            result += currValue;
-        }
-    }
-
-    return result;
+    
 }
 
 
