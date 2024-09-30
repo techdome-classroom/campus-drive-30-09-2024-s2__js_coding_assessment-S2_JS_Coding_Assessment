a5c72bd60ea7a06ce27b59bc40e5144080525f6e
mohammadsariqkhan <mohsariqkhan2004@gmail.com>
2024-09-30 14:14:32 +0530

    complete the code

--- program1.js
+++ program1.js
@@ -3,7 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+      let stack = [];
+      let map = {
+          '(': ')',
+          '[': ']',
+          '{': '}'
+      };
+      
     
+      for (let i = 0; i < s.length; i++) {
+
+          if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+              stack.push(s[i]);
+          } 
+          
+          else {
+              
+              if (stack.length === 0 || map[stack.pop()] !== s[i]) {
+                  return false;
+              }
+          }
+      }
+      return stack.length === 0;
+
 };
 
 module.exports = { isValid };
