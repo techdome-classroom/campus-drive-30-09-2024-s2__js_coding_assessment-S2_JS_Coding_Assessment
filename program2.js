0a65e015078c19da614a76a0f43684cdb54f7305
Yashraj360 <yashraj.sahu.jobsdoor360@gmail.com>
2024-09-30 14:09:55 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,41 +3,30 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const romanMap = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+    unordered_map<char, int> romanValues = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
     };
-    
-    let total = 0;
 
-    // Iterate over the string `s`
-    for (let i = 0; i < s.length; i++) {
-        // Get the value of the current character and the next character
-        let currentVal = romanMap[s[i]];
-        let nextVal = romanMap[s[i + 1]];
+    int result = 0;
 
-        // If the next value is greater than the current value, subtract current from total
-        if (nextVal > currentVal) {
-            total -= currentVal;
+    for (int i = s.length() - 1; i >= 0; i--) {
+        int currValue = romanValues[s[i]];
+
+        if (i < s.length() - 1 && currValue < romanValues[s[i + 1]]) {
+            result -= currValue;
         } else {
-            // Otherwise, add the current value to total
-            total += currentVal;
+            result += currValue;
         }
     }
-    
-    return total;
-}
 
-// Test cases
-console.log(romanToInt("III"));     // Output: 3
-console.log(romanToInt("LVIII"));   // Output: 58
-console.log(romanToInt("MCMXCIV")); // Output: 1994
-;
+    return result;
+}
 
 
 module.exports={romanToInt}
